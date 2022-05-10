e0a11b6ef2f4ccc3bc9face13b0bd5c297ef1d3b
vitor-henridias <vhsilva.mg@gmail.com>
2022-05-10 15:35:05 -0300

    Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,23 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
-
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './style.css';
 
 function Navbar() {
-   
-    return (
-        <header>
-        <nav>
-          <div>
-            <h1>DSMovie</h1>
-            <a href="https://github.com/vitor-henridias">
-              <div>
-                <GithubIcon />
-                <p>/devsuperior</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-      </header>
-    );
+
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/vitor-henridias">
+            <div className='dsmovie-contact-container'>
+              <GithubIcon />
+              <p className='dsmovie-contact-link'>/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 
 }
 
