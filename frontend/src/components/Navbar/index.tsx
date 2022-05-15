cbcbec7cf063498020f1c209be46d92bb17ed3bd
vitor-henridias <vhsilva.mg@gmail.com>
2022-05-14 21:29:21 -0300

    Alteração do link github

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
           <a href="https://github.com/vitor-henridias">
             <div className='dsmovie-contact-container'>
               <GithubIcon />
-              <p className='dsmovie-contact-link'>/devsuperior</p>
+              <p className='dsmovie-contact-link'>/vitor-henridias</p>
             </div>
           </a>
         </div>
